b97c1a91cf8fc509ec9dcb48cc8cf669835602bd
haleen24 <ya.barrymor@yandex.ru>
2023-02-21 19:27:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/sign-in')
+def sign_in():
+    return flask.render_template("sign_in.html")
+
+
+@app.route('/messanger')
+def messanger():
+    return flask.render_template("messanger.html")
+
+
+if __name__ == "__main__":
+    app.run("localhost", port=8080)
